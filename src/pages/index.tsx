0daf6f014b65a25b634793edcd84513c20ce1daf
eduardo3g <eduardosbrasil10@gmail.com>
2021-03-31 15:18:41 -0300

    style: create the initial version of the home page with css modules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
+import Head from 'next/head';
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
   return (
-    <h1>Hello world</h1>
-  )
-}
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+
+      <h1 className={styles.title}>Hello world</h1>
+    </>
+  );
+};
