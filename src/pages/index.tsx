2d01cc678d80558f57d766ea38f6dc22f1064e8f
eduardo3g <eduardosbrasil10@gmail.com>
2021-04-01 14:31:34 -0300

    feat: use static site generation (SSG) to load the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import styles from './home.module.scss';
 
@@ -37,7 +37,7 @@ export default function Home({ product }: HomeProps) {
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve(
     process.env.STRIPE_SUBSCRIPTION_PRODUCT_PRICE_ID)
   ;
@@ -54,5 +54,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
