1dc7d46fdd48c252be3e50ad30482d820a5a943b
eduardo3g <eduardosbrasil10@gmail.com>
2021-03-31 21:53:28 -0300

    feat: consume Stripe's API via SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import styles from './home.module.scss';
 
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  },
+};
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -16,14 +25,34 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90/month</span>
+            <span>for {product.amount}/month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve(
+    process.env.STRIPE_SUBSCRIPTION_PRODUCT_PRICE_ID)
+  ;
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100), // unit_amount is retrieved in cents from STRIPE
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
 };
