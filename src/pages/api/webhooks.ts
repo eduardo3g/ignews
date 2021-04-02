229a5efa84bcd8aaba2f6cfd9d7d2784d00a9a77
eduardo3g <eduardosbrasil10@gmail.com>
2021-04-02 11:43:16 -0300

    feat: listen to checkout.session.completed events from Stripe and store subscriptions on FaunaDB

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream';
 import Stripe from "stripe";
 import { stripe } from '../../services/stripe';
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -45,7 +46,23 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { type } = event;
 
     if (relevantEvents.has(type)) {
-      console.log('Received event', event);
+      try {
+        switch (type) {
+          case 'checkout.session.completed':
+            const checkoutSesion = event.data.object as Stripe.Checkout.Session;
+            
+            await saveSubscription(
+              checkoutSesion.subscription.toString(),
+              checkoutSesion.customer.toString()
+            );
+
+            break;
+          default:
+            throw new Error('Unhandled Stripe event.')
+        }
+      } catch (error) {
+        return res.json({ error: 'Webhook handler failed' });
+      }
     }
 
     res.json({ received: true })
