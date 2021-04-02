4ebc5d484ede8d3d20c912496051c1fba9484cd1
eduardo3g <eduardosbrasil10@gmail.com>
2021-04-02 19:09:59 -0300

    feat: create a separate component to identify active menus and apply a different style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,5 @@
-import Link from 'next/link';
-
 import { SignInButton } from '../SignInButton';
+import { ActiveLink } from '../ActiveLink';
 
 import styles from './styles.module.scss';
 
@@ -10,12 +9,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <Link href="/">
-            <a className={styles.active}>Home</a>
-          </Link>
-          <Link href="/posts" prefetch>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
