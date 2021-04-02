5579f8e758e8e63106bd8d4d57809fc0c5f7f58a
eduardo3g <eduardosbrasil10@gmail.com>
2021-04-02 18:49:36 -0300

    feat: listing posts from Prismic CMS

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,24 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+
+import { getPrismicClient } from '../../services/prismic';
+
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  summary: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[]
+};
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -10,28 +27,45 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>March 12th, 2021</time>
-            <strong>SaaS: Single tenant ou Multi-tenant, qual escolher?</strong>
-            <p>In this guide, you will learn how to create a Serverless applications with AWS Lambda functions. It's simple: build code and just pay for what you use!</p>
-          </a>
-          <a href="#">
-            <time>March 12th, 2021</time>
-            <strong>SaaS: Single tenant ou Multi-tenant, qual escolher?</strong>
-            <p>In this guide, you will learn how to create a Serverless applications with AWS Lambda functions. It's simple: build code and just pay for what you use!</p>
-          </a>
-          <a href="#">
-            <time>March 12th, 2021</time>
-            <strong>SaaS: Single tenant ou Multi-tenant, qual escolher?</strong>
-            <p>In this guide, you will learn how to create a Serverless applications with AWS Lambda functions. It's simple: build code and just pay for what you use!</p>
-          </a>
-          <a href="#">
-            <time>March 12th, 2021</time>
-            <strong>SaaS: Single tenant ou Multi-tenant, qual escolher?</strong>
-            <p>In this guide, you will learn how to create a Serverless applications with AWS Lambda functions. It's simple: build code and just pay for what you use!</p>
-          </a>
+          { posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.summary}</p>
+            </a>
+          )) }
         </div>
       </main>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.Predicates.at('document.type', 'post')
+  ], {
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100,
+  });
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      summary: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('en-US', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
 };
